190da3b1add523e89af5cd4378c5768f8c33e1b1
Eleonor2020 <reuk20077@mail.ru>
2026-04-18 17:08:29 +0700

    Bird + Fish

--- Laba-2-C#/Program.cs
+++ Laba-2-C#/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+public abstract class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Habitat { get; set; }
+    public string Diet { get; set; }
+
+    protected Animal(string name, int age, string habitat, string diet)
+    {
+        Name = name;
+        Age = age;
+        Habitat = habitat;
+        Diet = diet;
+    }
+
+    public virtual string Getinfo()
+    {
+        return $"Кличка: {Name}, Возраст: {Age}, Среда обитания: {Habitat}, Питание: {Diet}";
+    }
+
+    public class Mammal : Animal
+    {
+        public bool HasFur { get; set; }
+        public Mammal(string name, int age, string habitat, string diet, bool hasFur)
+            : base(name, age, habitat, diet)
+        {
+            HasFur = hasFur;
+        }
+        public override string Getinfo()
+        {
+            return $"{base.Getinfo()}, Тип: Млекопитающее, Шерсть: {(HasFur ? "есть" : "нет")}";
+        }
+    }
+}
+
+public class Bird : Animal
+{
+    public double WingSpan { get; set; }
+    public Bird(string name, int age, string habitat, string diet, double wingSpan)
+        : base(name, age, habitat, diet)
+    {
+        WingSpan = wingSpan;
+    }
+    public override string Getinfo()
+    {
+        return $"{base.Getinfo()}, Тип: Птица, Размах крыльев: {WingSpan} м";
+    }
+}
+
+public class Fish : Animal
+{
+    public string WaterType {  get; set; }
+    public Fish(string name, int age, string habitat, string diet, string waterType)
+        : base(name,age, habitat, diet)
+    {
+        WaterType = waterType;
+    }
+    public override string Getinfo()
+    {
+        return $"{base.Getinfo()}, Тип: Рыба, Тип воды: {WaterType}";
+    }
+}
+
+
+
